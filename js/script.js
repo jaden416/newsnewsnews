151ef90dce26ccea7830133ac3456b9a3f4968da
jaden416 <jaden416@outlook.com>
2022-05-28 20:35:51 -0400

    stat

---
+++ js/script.js
@@ -0,0 +1,33 @@
+window.onload = function(){
+
+
+var icon = document.getElementById('toggle')
+
+
+var count = 0;
+    var n = 2
+
+
+    icon.addEventListener("click", toggle);
+  
+
+    function toggle() {
+      if (count === 0) {
+
+        document.getElementById("mySidebar").style.left = "0px";
+        document.getElementById("main").style.marginLeft = "350px";
+      
+      }
+         else if (count === 1) {
+        document.getElementById("mySidebar").style.left = "-350px";
+        document.getElementById("main").style.marginLeft= "0";
+        count === 0;
+         }
+
+         
+      count += 1;
+      if (count === n)
+      count = 0;
+
+        }}
+      
