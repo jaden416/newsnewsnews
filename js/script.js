33030293ca3fa6464da6791cba9b2f2d7bc7a420
jaden416 <jaden416@outlook.com>
2022-06-07 11:58:43 -0400

    mobile bug fix

--- js/script.js
+++ js/script.js
@@ -13,12 +13,13 @@ var count = 0;
 
     function toggle() {
       if (count === 0) {
-
+        //Opens side bar
         document.getElementById("mySidebar").style.left = "0px";
         document.getElementById("main").style.marginLeft = "350px";
       
       }
          else if (count === 1) {
+           //Closes sidebar
         document.getElementById("mySidebar").style.left = "-350px";
         document.getElementById("main").style.marginLeft= "0";
         count === 0;
